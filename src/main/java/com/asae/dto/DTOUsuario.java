3573b11897f5dd6d3d45cdd16ae64204b1b14673
Duba94 <fabergarces@unicauca.edu.co>
2019-07-18 19:08:39 -0500

    Carpetas y paquetes

---
+++ src/main/java/com/asae/dto/DTOUsuario.java
@@ -0,0 +1,74 @@
+package com.asae.dto;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "objusuario", eager = true) 
+@RequestScoped 
+public class DTOUsuario  {
+
+	private int idUsuario;
+	private String nombre;
+	private String apellido;
+	private String fechaNacimiento;
+	private String correoElectronico;
+	private String contrasenia;
+	private String fechaRegistro;
+	private String fechaModificacion;
+	
+	
+	
+	public DTOUsuario() {
+		
+	}
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	public String getFechaRegistro() {
+		return fechaRegistro;
+	}
+	public void setFechaRegistro(String fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+	public String getFechaModificacion() {
+		return fechaModificacion;
+	}
+	public void setFechaModificacion(String fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
+	}
+	
+	
+}
